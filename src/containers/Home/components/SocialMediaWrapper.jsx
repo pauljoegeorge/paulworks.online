2fa3bacd0e13b53e4aa8d94518dd8cc63c2d014c
pauljoegeorge <pauljoegeorge@gmail.com>
2023-11-13 09:43:41 +0900

    fix social media icon color issue

--- src/containers/Home/components/SocialMediaWrapper.jsx
+++ src/containers/Home/components/SocialMediaWrapper.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const SocialMediaWrapper = styled.button`
   margin-top: 20px;
   margin-right: 5px;
-  background-color: black;
+  background-color: #0c1021;
   color: white;
   border: none;
   cursor: pointer;
