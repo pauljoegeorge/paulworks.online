5f4da5c596c2f49551877c673e123d6d7763871e
pauljoegeorge <pauljoegeorge@gmail.com>
2025-02-26 19:51:37 +0900

    recurrent expenses (#21)

---
+++ src/pages/RecurrentExpenses/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import RecurrentExpensesContainer from "../../containers/RecurrentExpenses";
+
+function RecurrentExpenses(props) {
+  return <RecurrentExpensesContainer {...props} />;
+}
+
+export default RecurrentExpenses;
